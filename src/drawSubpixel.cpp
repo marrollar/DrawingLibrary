dfb7b1b657c7527eff25d65de6b16ec7b19cc264
marrollar <ziyunye@gmail.com>
2021-08-05 09:25:22 -0400

    Added temporary drawing functions that are untested

--- src/drawSubpixel.cpp
+++ src/drawSubpixel.cpp
@@ -1,12 +1,11 @@
 #include "drawSubpixel.hpp"
 
-//#include "mathutils.hpp"
-
 using namespace cv;
 
 SubpixelWorkspace::SubpixelWorkspace(const cv::Mat &img, double row, double col, int scale, int buffer)
         : zoomScale{scale},
-          buffer{buffer} {
+          buffer{buffer},
+          retTemp(false) {
     double leftR = row - buffer;
     double leftC = col - buffer;
     double rightR = row + buffer;
@@ -72,6 +71,7 @@ SubpixelWorkspace::SubpixelWorkspace(const cv::Mat &img, double row, double col,
     resize(zoomedImage, zoomedImage, Size(), scale, scale, INTERPOLATION);
     leftRow = int(leftR);
     leftColumn = int(leftC);
+    temporaryImg = zoomedImage.clone();
 }
 
 void SubpixelWorkspace::drawCircle(double row, double col, double radius, const cv::Scalar &color) {
@@ -80,6 +80,18 @@ void SubpixelWorkspace::drawCircle(double row, double col, double radius, const 
     int zoomedRadius = int(round(radius * zoomScale));
 
     circle(zoomedImage, Point2d(zoomedCol, zoomedRow), zoomedRadius, color);
+}
+
+void SubpixelWorkspace::drawTempCircle(double row, double col, double radius, const cv::Scalar &color) {
+    int zoomedRow = int(round((row - leftRow) * zoomScale + zoomScale / 2.0));
+    int zoomedCol = int(round((col - leftColumn) * zoomScale + zoomScale / 2.0));
+    int zoomedRadius = int(round(radius * zoomScale));
+
+    if (not retTemp) {
+        temporaryImg = zoomedImage.clone();
+        retTemp = true;
+    }
+    circle(temporaryImg, Point2d(zoomedCol, zoomedRow), zoomedRadius, color);
 }
 
 void SubpixelWorkspace::drawPoint(double row, double col, const cv::Scalar &color) {
@@ -91,8 +103,20 @@ void SubpixelWorkspace::drawPoint(double row, double col, const cv::Scalar &colo
     circle(zoomedImage, Point2d(zoomedCol, zoomedRow), POINT_RADIUS, color, -1);
 }
 
-void
-SubpixelWorkspace::drawLine(double rowStart, double colStart, double rowEnd, double colEnd, const cv::Scalar &color) {
+void SubpixelWorkspace::drawTempPoint(double row, double col, const cv::Scalar &color) {
+    constexpr int POINT_RADIUS = 3;
+
+    int zoomedRow = int(round((row - leftRow) * zoomScale + zoomScale / 2.0));
+    int zoomedCol = int(round((col - leftColumn) * zoomScale + zoomScale / 2.0));
+
+    if (not retTemp) {
+        temporaryImg = zoomedImage.clone();
+        retTemp = true;
+    }
+    circle(temporaryImg, Point2d(zoomedCol, zoomedRow), POINT_RADIUS, color, -1);
+}
+
+void SubpixelWorkspace::drawLine(double rowStart, double colStart, double rowEnd, double colEnd, const cv::Scalar &color) {
     int zoomedRowStart = int(round((rowStart - leftRow) * zoomScale + zoomScale / 2.0));
     int zoomedColStart = int(round((colStart - leftColumn) * zoomScale + zoomScale / 2.0));
     int zoomedRowEnd = int(round((rowEnd - leftRow) * zoomScale + zoomScale / 2.0));
@@ -101,8 +125,20 @@ SubpixelWorkspace::drawLine(double rowStart, double colStart, double rowEnd, dou
     line(zoomedImage, Point2d(zoomedColStart, zoomedRowStart), Point2d(zoomedColEnd, zoomedRowEnd), color);
 }
 
-void
-SubpixelWorkspace::drawRect(const std::vector<cv::Point2d> &corners, const cv::Scalar &color, bool drawCorners) {
+void SubpixelWorkspace::drawTempLine(double rowStart, double colStart, double rowEnd, double colEnd, const cv::Scalar &color) {
+    int zoomedRowStart = int(round((rowStart - leftRow) * zoomScale + zoomScale / 2.0));
+    int zoomedColStart = int(round((colStart - leftColumn) * zoomScale + zoomScale / 2.0));
+    int zoomedRowEnd = int(round((rowEnd - leftRow) * zoomScale + zoomScale / 2.0));
+    int zoomedColEnd = int(round((colEnd - leftColumn) * zoomScale + zoomScale / 2.0));
+
+    if (not retTemp) {
+        temporaryImg = zoomedImage.clone();
+        retTemp = true;
+    }
+    line(temporaryImg, Point2d(zoomedColStart, zoomedRowStart), Point2d(zoomedColEnd, zoomedRowEnd), color);
+}
+
+void SubpixelWorkspace::drawRect(const std::vector<cv::Point2d> &corners, const cv::Scalar &color, bool drawCorners) {
     double rowStart;
     double colStart;
     double rowEnd;
@@ -131,6 +167,35 @@ SubpixelWorkspace::drawRect(const std::vector<cv::Point2d> &corners, const cv::S
     }
 }
 
+void SubpixelWorkspace::drawTempRect(const std::vector<cv::Point2d> &corners, const cv::Scalar &color, bool drawCorners) {
+    double rowStart;
+    double colStart;
+    double rowEnd;
+    double colEnd;
+
+    for (size_t i = 0; i < corners.size() - 1; i++) {
+        rowStart = corners[i].x;
+        colStart = corners[i].y;
+        rowEnd = corners[i + 1].x;
+        colEnd = corners[i + 1].y;
+
+        drawTempLine(rowStart, colStart, rowEnd, colEnd, color);
+    }
+    rowStart = corners[3].x;
+    colStart = corners[3].y;
+    rowEnd = corners[0].x;
+    colEnd = corners[0].y;
+
+    drawTempLine(rowStart, colStart, rowEnd, colEnd, color);
+
+    if (drawCorners) {
+        drawTempCircle(corners[0].x, corners[0].y, 5, COLOR_BLUE);
+        drawTempCircle(corners[1].x, corners[1].y, 5, COLOR_GREEN);
+        drawTempCircle(corners[2].x, corners[2].y, 5, COLOR_RED);
+        drawTempCircle(corners[3].x, corners[3].y, 5, COLOR_WHITE);
+    }
+}
+
 void SubpixelWorkspace::drawEllipse(double row, double col, double phi, double shortRadius, double longRadius,
                                     const cv::Scalar &color) {
     int zoomedRow = int(round((row - leftRow) * zoomScale + zoomScale / 2.0));
@@ -138,7 +203,21 @@ void SubpixelWorkspace::drawEllipse(double row, double col, double phi, double s
     int zoomedShortRad = int(round(shortRadius * zoomScale));
     int zoomedLongRad = int(round(longRadius * zoomScale));
 
-    ellipse(zoomedImage, Point2d(zoomedCol, zoomedRow), Size(zoomedShortRad, zoomedLongRad), (-phi) * 180 / CV_PI, 0, 360,
-            color);
+    ellipse(zoomedImage, Point2d(zoomedCol, zoomedRow), Size(zoomedShortRad, zoomedLongRad), (-phi) * 180 / CV_PI, 0, 360, color);
 }
+
+void SubpixelWorkspace::drawTempEllipse(double row, double col, double phi, double shortRadius, double longRadius,
+                                        const cv::Scalar &color) {
+    int zoomedRow = int(round((row - leftRow) * zoomScale + zoomScale / 2.0));
+    int zoomedCol = int(round((col - leftColumn) * zoomScale + zoomScale / 2.0));
+    int zoomedShortRad = int(round(shortRadius * zoomScale));
+    int zoomedLongRad = int(round(longRadius * zoomScale));
+
+    if (not retTemp) {
+        temporaryImg = zoomedImage.clone();
+        retTemp = true;
+    }
+    ellipse(temporaryImg, Point2d(zoomedCol, zoomedRow), Size(zoomedShortRad, zoomedLongRad), (-phi) * 180 / CV_PI, 0, 360, color);
+}
+
 
